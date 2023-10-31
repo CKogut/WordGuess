6c07f1cff8fb8fe3842f858c7294756abbb7bf7d
Corinne <CorinneKogut@gmail.com>
2023-10-31 17:26:25 -0400

    Added some pseudocode

--- src/main/java/com/github/zipcodewilmington/Hangman.java
+++ src/main/java/com/github/zipcodewilmington/Hangman.java
@@ -1,5 +1,6 @@
 package com.github.zipcodewilmington;
 
+import java.util.Scanner;
 
 /**
  * @author xt0fer
@@ -7,4 +8,29 @@ package com.github.zipcodewilmington;
  * @date 5/27/21 11:02 AM
  */
 public class Hangman {
+
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+
+        //Set the word to guess
+        String targetWord = "Guess";
+
+        //Explain the game
+        System.out.println("Can you guess what word I'm thinking?");
+        System.out.println("You have " + targetWord.length() + " tries!");
+
+
+        //While guessesRemain and !answered, ask for a letter
+            System.out.println("Enter a letter: ");
+
+            //Compare input to targetWord. If matches, show it to user hangman style.
+            //Ask for next guess
+
+        //if answered, show full word and tell them they won
+        //if out of guess, tell them they lost
+
+
+
+
+    }
 }
